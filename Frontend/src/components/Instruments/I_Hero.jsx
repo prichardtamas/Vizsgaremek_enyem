038d6bf34835b66cc2030649293abb7f8f7f864e
P. Richárd Tamás <paraoanurichard@gmail.com>
2026-02-26 12:52:41 +0100

    Változások

--- Frontend/src/components/Instruments/I_Hero.jsx
+++ Frontend/src/components/Instruments/I_Hero.jsx
@@ -5,12 +5,8 @@ import { useNavigate } from 'react-router-dom';
 const HeroSection = () => {
     const navigate = useNavigate();
 
-    const handleConsultClick = () => {
-        alert("Köszönjük érdeklődésed a hangszer konzultációért! Hamarosan felvesszük veled a kapcsolatot a megadott információkkal.");
-    };
-
     const handleRentClick = () => {
-        navigate('/kolcsonzes');
+        navigate('/Rent');
     };
 
     return (
@@ -22,9 +18,6 @@ const HeroSection = () => {
                             <h1 className="mb-4">A Hangszerek <span>Világa</span></h1>
                             <p className="mb-4">Fedezd fel a különböző hangszercsaládokat, jellemzőiket és működésüket. Ismerd meg, melyik hangszert milyen zenei stílusokhoz használják és miben különböznek egymástól.</p>
                             <div className="hero-buttons d-flex justify-content-center gap-3">
-                                <button className="btn btn-primary" onClick={handleConsultClick}>
-                                    Hangszer Konzultáció
-                                </button>
                                 <button className="btn btn-outline-primary" onClick={handleRentClick}>
                                     Hangszer Kölcsönzés
                                 </button>
